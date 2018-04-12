9e1fa0422f078b62c1911009053a44946c506608
Manvel Saroyan <saroyanm@gmail.com>
2018-04-12 22:57:52 +0200

    #5 - made the header responsive

--- public/js/main.js
+++ public/js/main.js
@@ -8,3 +8,9 @@ langaugeSelector.addEventListener("change", e => {
   let port = ":" + window.location.port;
   window.location.href = protocol + "//" + hostname + port + "/" +  locale + page;
 });
+
+const hamburger = document.querySelector("#hamburger");
+hamburger.addEventListener("click", e =>
+{
+  document.querySelector("#hamburger-menu").classList.toggle("visible");
+});
