3ce92f653b24640c7e5b02364b4682d786133d87
Manvel Saroyan <saroyanm@gmail.com>
2018-04-28 01:14:43 +0200

    Added fixed width problem to the pain-points

---
+++ public/devdays/pain-points.js
@@ -0,0 +1,53 @@
+const doneButton = document.getElementById("done-button");
+
+
+document.body.addEventListener("click", onClick, false);
+document.body.addEventListener("change", onChange, false);
+
+function onClick(e)
+{
+  let actions = findParentData(e.target, "action", false);
+  if (!actions)
+    return;
+
+  actions = actions.split(",");
+  for (let action of actions)
+  {
+    execAction(action, e.target);
+  }
+}
+
+function onChange(e)
+{
+  let actions = findParentData(e.target, "change", false);
+  if (!actions)
+    return;
+  actions = actions.split(",");
+  for (let action of actions)
+  {
+    execAction(action, e.target);
+  }
+}
+
+function findParentData(element, dataName, returnElement)
+{
+  element = element.closest(`[data-${dataName}]`);
+  if (!element)
+    return null;
+  if (returnElement)
+    return element;
+  return element.getAttribute(`data-${dataName}`);
+}
+
+function execAction(action, element)
+{
+  switch (action)
+  {
+    case "set-button-german":
+      doneButton.textContent = "Fertigstellen";
+      break;
+    case "add-flexible-width":
+      doneButton.style.width = "100%";
+      break;
+  }
+}
