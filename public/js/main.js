89af6e43375b0848c0ba54c0dd430c1ab3cdd385
Manvel Saroyan <saroyanm@gmail.com>
2018-05-25 14:12:11 +0300

    #25 - Added logic for closing the context menu on clicking outside and esc

--- public/js/main.js
+++ public/js/main.js
@@ -18,12 +18,37 @@ hamburger.addEventListener("click", e =>
   document.querySelector("#hamburger-menu").classList.toggle("visible");
 });
 
-const contextMenus = document.querySelectorAll(".contextMenu");
-for (const contextMenu of contextMenus)
+document.body.addEventListener("click", onClick, false);
+document.body.addEventListener("keyup", onKeyUp, false);
+function onClick(e)
 {
-  const contextMenuButton = contextMenu.querySelector("button");
-  contextMenuButton.addEventListener("click", (e) =>
+  const element = e.target.closest("[data-action]");
+  if (!element)
+    closeVisibleContext();
+
+  if (element)
+    execAction(element.dataset.action, element);
+}
+
+function onKeyUp(e)
+{
+  if (e.key == "Escape")
+    closeVisibleContext();
+}
+
+function closeVisibleContext()
+{
+  const menus = document.querySelectorAll(".contextMenu.visible");
+  menus.forEach((menu) => menu.classList.remove("visible"));
+}
+
+function execAction(action, element)
+{
+  switch (action)
   {
-    contextMenu.classList.toggle("visible");
-  });
+    case "toggle-context":
+      const contextMenus = element.closest(".contextMenu");
+      contextMenus.classList.toggle("visible");
+      break;
+  }
 }
