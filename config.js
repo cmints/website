101e2834d4a9efb6cbd2e05d0173902406741d1e
Manvel Saroyan <saroyanm@gmail.com>
2018-08-02 21:23:27 +0200

    Moved crowdin projectId into the project

--- config.js
+++ config.js
@@ -1,3 +1,15 @@
+// See https://markdown-it.github.io/markdown-it/#MarkdownIt.new
+const markdownOptions = {};
+
+const defaultLocale = "en";
+
+const port = {
+  https: 4000,
+  http: 3000
+};
+
+const crowdinId = "cmints-website";
+
 const templateData =
 {
   site: {
@@ -5,7 +17,7 @@ const templateData =
     title: "CMintS",
     description: "CMS created with the internationalization in mind",
     github: "https://github.com/Manvel/cmints-website",
-    crowdin: "https://crowdin.com/translate/cmints-website",
+    crowdin: `https://crowdin.com/translate/${crowdinId}`,
     localeMap: {
       "en": {name: "English", region: "en_US"},
       "de": {name: "Deutsch", region: "de_DE", crowdin: "en-de"},
@@ -22,17 +34,5 @@ const templateData =
   }
 };
 
-// See https://markdown-it.github.io/markdown-it/#MarkdownIt.new
-const markdownOptions = {};
-
-const defaultLocale = "en";
-
-const port = {
-  https: 4000,
-  http: 3000
-};
-
-exports.templateData = templateData;
-exports.markdownOptions = markdownOptions;
-exports.defaultLocale = defaultLocale;
-exports.port = port;
+module.exports = {templateData, markdownOptions, defaultLocale, port,
+  crowdinId};
