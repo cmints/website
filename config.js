c061aed3373f951a6e66b75701c11565bbc16fbf
Manvel Saroyan <saroyanm@gmail.com>
2018-08-11 18:30:14 +0200

    Update config to reflect i18nOptions

--- config.js
+++ config.js
@@ -1,15 +1,16 @@
 // See https://markdown-it.github.io/markdown-it/#MarkdownIt.new
 const markdownOptions = {};
 
-const defaultLocale = "en";
-
 const port = {
   https: 4000,
   http: 3000
 };
 const hostname = "0.0.0.0";
 
-const crowdinId = "cmints-website";
+const i18nOptions = {
+  defaultLocale: "en",
+  crowdinId: "cmints-website"
+};
 
 const templateData =
 {
@@ -18,7 +19,7 @@ const templateData =
     title: "CMintS",
     description: "CMS created with the internationalization in mind",
     github: "https://github.com/Manvel/cmints-website",
-    crowdin: `https://crowdin.com/translate/${crowdinId}`,
+    crowdin: `https://crowdin.com/translate/${i18nOptions.crowdinId}`,
     localeMap: {
       "en": {name: "English", region: "en_US"},
       "de": {name: "Deutsch", region: "de_DE", crowdin: "en-de"},
@@ -35,5 +36,5 @@ const templateData =
   }
 };
 
-module.exports = {templateData, markdownOptions, defaultLocale, port,
-  hostname, crowdinId};
+module.exports = {templateData, markdownOptions, i18nOptions, port,
+  hostname};
